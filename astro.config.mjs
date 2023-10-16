198f439342d957d1de3a286d78881ee1469280ac
PeWe79 <poer.one79@gmail.com>
2023-10-16 19:11:58 +0700

    Add edit link.

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,6 +27,9 @@ export default defineConfig({
         discord: 'https://discord.gg/azuracast',
         github: 'https://github.com/pewe79/cloudmucast',
       },
+      editLink: {
+        baseUrl: 'https://github.com/azuracast/azuracast.com/tree/main/src/content/docs/docs',
+      },
       sidebar: [
         { label: 'Home', link: '/' },
         { label: 'About CloudmuCast', link: '/docs' },
